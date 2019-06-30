6076c47479d3e05dc97079572192c3bd9519cd1b
mcalus3 <marek-calus@wp.pl>
2019-06-30 19:53:59 +0200

    Improve "About" component.

--- src/App/appNavigation/About.tsx
+++ src/App/appNavigation/About.tsx
@@ -1,33 +1,47 @@
 import React from 'react';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import {
-  DialogContent,
-  DialogContentText,
   DialogActions,
-  Button
+  Button,
+  Slide,
+  Typography,
+  DialogTitle,
+  DialogContent
 } from '@material-ui/core';
+import { TransitionProps } from 'react-transition-group/Transition';
+
+const Transition = React.forwardRef<unknown, TransitionProps>(
+  function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  }
+);
 
 export interface AboutProps {
   open: boolean;
   onClose: () => void;
 }
 
-export default function About(props: AboutProps) {
-  const { onClose, ...other } = props;
-
+export default function About({ open, onClose }: AboutProps) {
   return (
-    <Dialog onClose={onClose} aria-labelledby="about" {...other}>
-      <DialogTitle id="about">About open-fraksl</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="about-description">
+    <Dialog
+      onClose={onClose}
+      open={open}
+      aria-labelledby="about-title"
+      aria-describedby="about-description"
+      //@ts-ignore
+      TransitionComponent={Transition}
+      keepMounted
+    >
+      <DialogTitle id="about-title">About open-fraksl</DialogTitle>
+      <DialogContent dividers>
+        <Typography gutterBottom>
           open-fraksl is a tool for you to create beautiful, yet simple
           generative art. Its aim is to allow user to explore unlimited
           possibilities of shaping his own fractals. I decided to make this app
           after many hours of playing with a mobile app, called Fraksl. I was
           wondering - would I be able to write down a procedure that would
           generate such a fractal?
-        </DialogContentText>
+        </Typography>
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose} color="primary">
